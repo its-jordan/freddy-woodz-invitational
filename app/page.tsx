a4060399349df005be7788419fdd124530ffe6d4
its_jordan <s.schultzjordan.jordan@gmail.com>
2023-08-19 19:13:35 -0400

    Updated Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-import Nav from '@/components/nav';
+import Tokotoro from '@/teams/tokotoro';
+import Ifurgat from '@/teams/ifurgat';
+import SeanBoyQ from '@/teams/seanboyq';
+import Resolamxxy from '@/teams/resolamxxy';
+import JDougs from '@/teams/jdougs';
+import Dom from '@/teams/dom';
+import Danknett from '@/teams/danknett';
+import Foxish from '@/teams/foxish';
+import Itsjordan from '@/teams/itsjordan';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="mb-16">
+      <div className="flex flex-col gap-4">
+        <SeanBoyQ />
+        <Resolamxxy />
+        <JDougs />
+        <Dom />
+        <Tokotoro />
+        <Danknett />
+        <Ifurgat />
+        <Foxish />
+        <Itsjordan />
+      </div>
+    </main>
   );
 }
