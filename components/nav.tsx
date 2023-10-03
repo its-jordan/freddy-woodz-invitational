9596b5d56fc38065f6c8f5d6ae2010f8cf579849
its_jordan <s.schultzjordan.jordan@gmail.com>
2023-10-03 02:55:29 -0400

    Added mobile support, still wip

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Link from 'next/link';
 // @ts-ignore
 import Data from '../data/replays.yaml';
+import { useState } from 'react';
 
 export const navLinks = [
   {
@@ -42,55 +45,82 @@ export const navLinks = [
 ];
 
 function Nav() {
+  const [nav, setNav] = useState(false);
   const replayData = Data;
   return (
-    <nav>
-      <Link className='nav-header' href='./'>
-        Teams
-      </Link>
-      {navLinks.map((link, index) => {
-        return (
-          <Link
-            // @ts-ignore
-            href={link?.path}
-            key={index}
-            data-player={link?.path.split('#')[1]}>
-            {link?.name}
-          </Link>
-        );
-      })}
-      <div className='nav-divider'></div>
-      <ul className='replays-dropdown-container'>
-        <Link className='replays-dropdown-header' href='./replays'>
-          Replays
+    <>
+      <button
+        className={
+          nav
+            ? 'nav-button-mobile closed hide'
+            : 'nav-button-mobile closed show'
+        }
+        onClick={() => {
+          setNav(true);
+        }}
+        onTouchStart={() => {
+          setNav(true);
+        }}>
+        Open Nav 🠞
+      </button>
+      <nav className={nav ? 'navigation visible' : 'navigation hidden'}>
+        <button
+          className='nav-button-mobile open'
+          onClick={() => {
+            setNav(false);
+          }}
+          onTouchStart={() => {
+            setNav(false);
+          }}>
+          🠜 Close Nav
+        </button>
+        <Link className='nav-header' href='./'>
+          Teams
         </Link>
-        <ul className='replays-dropdown'>
-          {replayData
-            .sort((a: any, b: any) =>
-              a.player1 > b.player1 ? 1 : b.player1 > a.player1 ? -1 : 0
-            )
-            .map((player: any, index: any) => {
-              return (
-                <Link
-                  href={`./replays/#${player.player1Id}-${player.player2Id}-${player.game}`}
-                  data-player1={`${player.player1Id}`}
-                  className='replay-dropdown-link'
-                  key={index}>
-                  {player.player1.charAt(0).toUpperCase() +
-                    player.player1.slice(1)}{' '}
-                  vs.{' '}
-                  {player.player2.charAt(0).toUpperCase() +
-                    player.player2.slice(1)}{' '}
-                  - G{player.game}
-                </Link>
-              );
-            })}
+        {navLinks.map((link, index) => {
+          return (
+            <Link
+              // @ts-ignore
+              href={link?.path}
+              key={index}
+              data-player={link?.path.split('#')[1]}>
+              {link?.name}
+            </Link>
+          );
+        })}
+        <div className='nav-divider'></div>
+        <ul className='replays-dropdown-container'>
+          <Link className='replays-dropdown-header' href='./replays'>
+            Replays
+          </Link>
+          <ul className='replays-dropdown'>
+            {replayData
+              .sort((a: any, b: any) =>
+                a.player1 > b.player1 ? 1 : b.player1 > a.player1 ? -1 : 0
+              )
+              .map((player: any, index: any) => {
+                return (
+                  <Link
+                    href={`./replays/#${player.player1Id}-${player.player2Id}-${player.game}`}
+                    data-player1={`${player.player1Id}`}
+                    className='replay-dropdown-link'
+                    key={index}>
+                    {player.player1.charAt(0).toUpperCase() +
+                      player.player1.slice(1)}{' '}
+                    vs.{' '}
+                    {player.player2.charAt(0).toUpperCase() +
+                      player.player2.slice(1)}{' '}
+                    - G{player.game}
+                  </Link>
+                );
+              })}
+          </ul>
         </ul>
-      </ul>
-      <Link href='./scores'>Scores</Link>
-      <Link href='./meme'>Discord Tiers</Link>
-      {/* <Link href='./archive'>Season 1 Archive</Link> */}
-    </nav>
+        <Link href='./scores'>Scores</Link>
+        <Link href='./meme'>Discord Tiers</Link>
+        {/* <Link href='./archive'>Season 1 Archive</Link> */}
+      </nav>
+    </>
   );
 }
 
