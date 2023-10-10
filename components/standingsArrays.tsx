e4adcccd7912ed96b05123bd31d6ad73f22e002f
its_jordan <s.schultzjordan.jordan@gmail.com>
2023-10-10 18:58:08 -0400

    made some updates for standings, css

--- components/standingsArrays.tsx
+++ components/standingsArrays.tsx
@@ -58,45 +58,6 @@ export default async function Standings() {
       eachMatch[35]?.match.winner_id,
     ];
 
-    let winnerPointsArray = [
-      eachMatch[0]?.match.winner_id,
-      eachMatch[1]?.match.winner_id,
-      eachMatch[2]?.match.winner_id,
-      eachMatch[3]?.match.winner_id,
-      eachMatch[4]?.match.winner_id,
-      eachMatch[5]?.match.winner_id,
-      eachMatch[6]?.match.winner_id,
-      eachMatch[7]?.match.winner_id,
-      eachMatch[8]?.match.winner_id,
-      eachMatch[9]?.match.winner_id,
-      eachMatch[10]?.match.winner_id,
-      eachMatch[11]?.match.winner_id,
-      eachMatch[12]?.match.winner_id,
-      eachMatch[13]?.match.winner_id,
-      eachMatch[14]?.match.winner_id,
-      eachMatch[15]?.match.winner_id,
-      eachMatch[16]?.match.winner_id,
-      eachMatch[17]?.match.winner_id,
-      eachMatch[18]?.match.winner_id,
-      eachMatch[19]?.match.winner_id,
-      eachMatch[20]?.match.winner_id,
-      eachMatch[21]?.match.winner_id,
-      eachMatch[22]?.match.winner_id,
-      eachMatch[23]?.match.winner_id,
-      eachMatch[24]?.match.winner_id,
-      eachMatch[25]?.match.winner_id,
-      eachMatch[26]?.match.winner_id,
-      eachMatch[27]?.match.winner_id,
-      eachMatch[28]?.match.winner_id,
-      eachMatch[29]?.match.winner_id,
-      eachMatch[30]?.match.winner_id,
-      eachMatch[31]?.match.winner_id,
-      eachMatch[32]?.match.winner_id,
-      eachMatch[33]?.match.winner_id,
-      eachMatch[34]?.match.winner_id,
-      eachMatch[35]?.match.winner_id,
-    ];
-
     winnerArray.forEach(function (x) {
       winners[x] = (winners[x] || 0) + 1;
     });
@@ -162,46 +123,332 @@ export default async function Standings() {
 
     const loserScores = {};
 
-    let loserScore = [
-      eachMatch[0]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[1]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[2]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[3]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[4]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[5]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[6]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[7]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[8]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[9]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[10]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[11]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[12]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[13]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[14]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[15]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[16]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[17]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[18]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[19]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[20]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[21]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[22]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[23]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[24]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[25]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[26]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[27]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[28]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[29]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[30]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[31]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[32]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[33]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[34]?.match.scores_csv.replace('2', '').replace('-', ''),
-      eachMatch[35]?.match.scores_csv.replace('2', '').replace('-', ''),
+    let matchArray = [
+      {
+        loser: {
+          id: eachMatch[0]?.match.loser_id,
+          score: eachMatch[0]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[0]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[1]?.match.loser_id,
+          score: eachMatch[1]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[1]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[2]?.match.loser_id,
+          score: eachMatch[2]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[2]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[3]?.match.loser_id,
+          score: eachMatch[3]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[3]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[4]?.match.loser_id,
+          score: eachMatch[4]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[4]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[5]?.match.loser_id,
+          score: eachMatch[5]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[5]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[6]?.match.loser_id,
+          score: eachMatch[6]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[6]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[7]?.match.loser_id,
+          score: eachMatch[7]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[7]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[8]?.match.loser_id,
+          score: eachMatch[8]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[8]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[9]?.match.loser_id,
+          score: eachMatch[9]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[9]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[10]?.match.loser_id,
+          score: eachMatch[10]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[10]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[11]?.match.loser_id,
+          score: eachMatch[11]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[11]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[12]?.match.loser_id,
+          score: eachMatch[12]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[12]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[13]?.match.loser_id,
+          score: eachMatch[13]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[13]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[14]?.match.loser_id,
+          score: eachMatch[14]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[14]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[15]?.match.loser_id,
+          score: eachMatch[15]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[15]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[16]?.match.loser_id,
+          score: eachMatch[16]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[16]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[17]?.match.loser_id,
+          score: eachMatch[17]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[17]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[18]?.match.loser_id,
+          score: eachMatch[18]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[18]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[19]?.match.loser_id,
+          score: eachMatch[19]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[19]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[20]?.match.loser_id,
+          score: eachMatch[20]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[20]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[21]?.match.loser_id,
+          score: eachMatch[21]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[21]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[22]?.match.loser_id,
+          score: eachMatch[22]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[22]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[23]?.match.loser_id,
+          score: eachMatch[23]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[23]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[24]?.match.loser_id,
+          score: eachMatch[24]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[24]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[25]?.match.loser_id,
+          score: eachMatch[25]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[25]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[26]?.match.loser_id,
+          score: eachMatch[26]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[26]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[27]?.match.loser_id,
+          score: eachMatch[27]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[27]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[28]?.match.loser_id,
+          score: eachMatch[28]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[28]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[29]?.match.loser_id,
+          score: eachMatch[29]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[29]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[30]?.match.loser_id,
+          score: eachMatch[30]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[30]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[31]?.match.loser_id,
+          score: eachMatch[31]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[31]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[32]?.match.loser_id,
+          score: eachMatch[32]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[32]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[33]?.match.loser_id,
+          score: eachMatch[33]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[33]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[34]?.match.loser_id,
+          score: eachMatch[34]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[34]?.match.winner_id },
+      },
+      {
+        loser: {
+          id: eachMatch[35]?.match.loser_id,
+          score: eachMatch[35]?.match.scores_csv
+            .replace('2', '')
+            .replace('-', ''),
+        },
+        winner: { id: eachMatch[35]?.match.winner_id },
+      },
     ];
-
-    console.log(loserScore);
 
     loserArray.forEach(function (x) {
       losers[x] = (losers[x] || 0) + 1;
@@ -218,8 +465,6 @@ export default async function Standings() {
     let currentStandings = winnerValues.map((item, i) =>
       Object.assign({}, item, loserValues[i])
     );
-
-    console.log(currentStandings);
 
     const arrOfIds = [
       'ifurgat',
@@ -304,7 +549,12 @@ export default async function Standings() {
               className='standings-record'
               data-playoffs={player.wins < 2 ? 'no' : 'yes'}
               key={player.id}>
-              <div className='standings-name'>{player.name}</div>
+              <div className='standings-name'>
+                {replaceName(player.id)}
+                <div className='standings-playoffs'>
+                  {returnPlayoffs(player.id)}
+                </div>
+              </div>
               <div className='standings-wins'>{player.wins}</div>
               <div className='standings-losses'>{player.losses}</div>
               <div className='standings-points'>{player.points}</div>
@@ -312,6 +562,20 @@ export default async function Standings() {
             </Link>
           );
         })}
+        <div className='standings-guide'>
+          <div className='qualified'>
+            <div className='standings-guide-symbol'>Q</div>
+            <div className='standings-guide-meaning'>
+              Qualified for Playoffs
+            </div>
+          </div>
+          <div className='eliminated'>
+            <div className='standings-guide-symbol'>E</div>
+            <div className='standings-guide-meaning'>
+              Eliminated from Playoff Contention
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -324,8 +588,7 @@ export default async function Standings() {
     );
     return (
       <div className='player-name' data-player={participants.participant.id}>
-        {participants.participant.name.charAt(0).toUpperCase() +
-          participants.participant.name.slice(1)}
+        {replaceName(participants.participant.id)}
       </div>
     );
   }
@@ -353,6 +616,52 @@ export default async function Standings() {
     }
   }
 
+  function returnPlayoffs(e: any) {
+    let playerId = e;
+    if (playerId == '208943681') {
+      return 'E';
+    } else if (playerId == '208943656') {
+      return 'Q';
+    } else if (playerId == '208943645') {
+      return 'Q';
+    } else if (playerId == '208943667') {
+      return 'Q';
+    } else if (playerId == '208943657') {
+      return 'Q';
+    } else if (playerId == '208943647') {
+      return 'Q';
+    } else if (playerId == '208943648') {
+      return 'E';
+    } else if (playerId == '208943679') {
+      return 'E';
+    } else if (playerId == '208943635') {
+      return 'Q';
+    }
+  }
+
+  function replaceName(e: any) {
+    let playerId = e;
+    if (playerId == '208943681') {
+      return 'its_jordan';
+    } else if (playerId == '208943656') {
+      return 'Castleflutes';
+    } else if (playerId == '208943645') {
+      return 'SeanBoyQ';
+    } else if (playerId == '208943667') {
+      return 'Danknett';
+    } else if (playerId == '208943657') {
+      return 'Tokotoro';
+    } else if (playerId == '208943647') {
+      return 'resolamxxy';
+    } else if (playerId == '208943648') {
+      return 'DTBaggins';
+    } else if (playerId == '208943679') {
+      return 'Foxish';
+    } else if (playerId == '208943635') {
+      return 'iFurgat';
+    }
+  }
+
   return (
     <div className='score-container'>
       {DisplayWinners()}
@@ -374,7 +683,6 @@ export default async function Standings() {
                 data-player={value.match.player1_id}
                 data-score={value.match.scores_csv.charAt(0)}>
                 <div className='player-container'>
-                  <div className='player-heading'>Player 1</div>
                   {displayName(value.match.player1_id)}
                 </div>
                 <div
@@ -388,7 +696,6 @@ export default async function Standings() {
                 data-player={value.match.player2_id}
                 data-score={value.match.scores_csv.charAt(2)}>
                 <div className='player-container'>
-                  <div className='player-heading'>Player 2</div>
                   {displayName(value.match.player2_id)}
                 </div>
                 <div
