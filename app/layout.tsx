a3361f76e0b86303b98aae34b358ea65b60c6b05
its_jordan <s.schultzjordan.jordan@gmail.com>
2023-10-12 17:51:55 -0400

    Added configurations for PWAs (should work?)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,49 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+const APP_NAME = 'Freddy Woodz Invitational';
+const APP_DEFAULT_TITLE = 'Freddy Woodz Invitational';
+const APP_TITLE_TEMPLATE = '%s - Freddy Woodz Invitational';
+const APP_DESCRIPTION =
+  'Team information for Freddy Woodz Invitational Pokemon Showdown Tournament';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Freddy Woodz Invitational',
-  description:
-    'Team information for Freddy Woodz Invitational Pokemon Showdown Tournament',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: '/manifest.json',
+  themeColor: '#FFFFFF',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverActions: true,
   },
 };
 
-module.exports = {
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+  },
   images: {
     remotePatterns: [
       {
@@ -21,4 +32,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
